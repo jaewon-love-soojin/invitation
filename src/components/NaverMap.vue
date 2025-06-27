47b4de1d277520aa0891e07503b31a715f68e7fa
jwchoi <jwchoi.do@gmail.com>
2025-06-28 05:53:51 +0900

    Fix syntax error

--- src/components/NaverMap.vue
+++ src/components/NaverMap.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="map" class="map-container">
-    <img :src="https://maps.apigw.ntruss.com/map-static/v2/raster-cors?w=300&h=300&center=127.1083402,37.3995154&level=16&X-NCP-APIGW-API-KEY-ID="apiKey">
+    <img :src="imagePath()" />
   </div>
 </template>
 
 <script setup>
 const apiKey = () => {
-  return import.meta.env.VITE_X_NCP_APIGW_API_KEY_ID;
+return `https://maps.apigw.ntruss.com/map-static/v2/raster-cors?w=300&h=300&center=127.1083402,37.3995154&level=16&X-NCP-APIGW-API-KEY-ID=${import.meta.env.VITE_X_NCP_APIGW_API_KEY_ID}`;
 };
 </script>
 
