b56d675ca2a9e0ced63567592a85b796f9111a71
jwchoi <jwchoi.do@gmail.com>
2025-06-28 06:02:11 +0900

    Fix typo

--- src/components/NaverMap.vue
+++ src/components/NaverMap.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup>
-const apiKey = () => {
+const imagePath = () => {
 return `https://maps.apigw.ntruss.com/map-static/v2/raster-cors?w=300&h=300&center=127.1083402,37.3995154&level=16&X-NCP-APIGW-API-KEY-ID=${import.meta.env.VITE_X_NCP_APIGW_API_KEY_ID}`;
 };
 </script>
